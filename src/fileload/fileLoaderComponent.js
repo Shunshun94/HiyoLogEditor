2dd43bed94997be2b0ffacc3a82068445c26e6ac
Shunshun94 <shunshun9460@gmail.com>
2021-04-01 00:36:21 +0900

    separate file loader from index

---
+++ src/fileload/fileLoaderComponent.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import FileLoader from './fileLoad.js';
+
+class FileLoaderComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            bgColor: 'white'
+        };
+    }
+
+    onDrop(e) {
+        e.preventDefault();
+        this.setState({
+            bgColor: 'white'
+        });
+        const self = this;
+        FileLoader.onDrop(e).then(self.props.onFileLoaded);
+    }
+
+    onDragLeave(e) {
+        this.setState({
+            bgColor: 'white'
+        });
+    }
+ 
+    onDragOver(e) {
+        e.stopPropagation();
+        e.preventDefault();
+        this.setState({
+            bgColor: 'blue'
+        });
+    }
+
+    render() {
+        const classes = `io-github-shunshun94-trpg-logEditor-FileLoader io-github-shunshun94-trpg-logEditor-FileLoader-${this.state.bgColor}`;
+        return (
+            <div
+                className={classes}
+                onDrop={(e)=>this.onDrop(e)}
+                onDragLeave={(e)=>{this.onDragLeave(e)}}
+                onDragOver={(e)=>{this.onDragOver(e)}}
+            >
+                ログの HTML ないしユドナリウムの部屋の zip  を Drag/Drop するか、以下からアップロードしてください<br/>
+                <input
+                    type="file"
+                    name="io-github-shunshun94-trpg-logEditor-FileLoader-InputFile"
+                    id="io-github-shunshun94-trpg-logEditor-FileLoader-InputFile" 
+                    onInput={(e)=>FileLoader.onInput(e)}
+                    />
+            </div>
+        );
+    }
+}
+
+export default FileLoaderComponent;
--- src/index.js
+++ src/index.js
@@ -3,61 +3,9 @@ import ReactDOM from 'react-dom';
 import './css/logEditor.css';
 import './css/darkmode.css';
 import './css/wideScreenMode.css';
-import FileLoader from './fileload/fileLoad.js';
+import FileLoaderComponent from './fileload/fileLoaderComponent.js';
 
 const e = React.createElement;
-
-class FileLoaderComponent extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            bgColor: 'white'
-        };
-    }
-
-    onDrop(e) {
-        e.preventDefault();
-        this.setState({
-            bgColor: 'white'
-        });
-        const self = this;
-        FileLoader.onDrop(e).then(self.props.onFileLoaded);
-    }
-
-    onDragLeave(e) {
-        this.setState({
-            bgColor: 'white'
-        });
-    }
- 
-    onDragOver(e) {
-        e.stopPropagation();
-        e.preventDefault();
-        this.setState({
-            bgColor: 'blue'
-        });
-    }
-
-    render() {
-        const classes = `io-github-shunshun94-trpg-logEditor-FileLoader io-github-shunshun94-trpg-logEditor-FileLoader-${this.state.bgColor}`;
-        return (
-            <div
-                className={classes}
-                onDrop={(e)=>this.onDrop(e)}
-                onDragLeave={(e)=>{this.onDragLeave(e)}}
-                onDragOver={(e)=>{this.onDragOver(e)}}
-            >
-                ログの HTML ないしユドナリウムの部屋の zip  を Drag/Drop するか、以下からアップロードしてください<br/>
-                <input
-                    type="file"
-                    name="io-github-shunshun94-trpg-logEditor-FileLoader-InputFile"
-                    id="io-github-shunshun94-trpg-logEditor-FileLoader-InputFile" 
-                    onInput={(e)=>FileLoader.onInput(e)}
-                    />
-            </div>
-        );
-    }
-}
 
 class LogEditor extends React.Component {
     constructor(props) {
